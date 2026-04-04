2943cf7e211e0e5b8e18541c1394bf6f66522a9b
JuanHerS05 <juan.herrera14@uptc.edu.co>
2026-04-03 19:57:42 -0500

    ui-buttons-with-icons

--- src/main/java/co/edu/uptc/automatonsimulationservice/ui/services/ApplicationBootstrapService.java
+++ src/main/java/co/edu/uptc/automatonsimulationservice/ui/services/ApplicationBootstrapService.java
@@ -12,7 +12,7 @@ public class ApplicationBootstrapService {
                 ApplicationBootstrapService.class.getResource("/co/edu/uptc/automatonsimulationservice/ui/views/main-view.fxml")
             );
             Scene scene = new Scene(loader.load(), 1450, 780);
-            stage.setTitle("Simulador y Analizador Universal de Automatas Finitos");
+            stage.setTitle("SIMULADOR DE AUTÓMATAS FINITOS");
             stage.setScene(scene);
             stage.show();
         } catch (Exception exception) {
