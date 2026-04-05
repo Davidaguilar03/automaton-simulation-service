1e1744dce3fb74876e97d636f023cd0214496376
JuanHerS05 <juan.herrera14@uptc.edu.co>
2026-04-05 11:20:23 -0500

    ui-program-logo

--- src/main/java/co/edu/uptc/automatonsimulationservice/ui/services/ApplicationBootstrapService.java
+++ src/main/java/co/edu/uptc/automatonsimulationservice/ui/services/ApplicationBootstrapService.java
@@ -2,6 +2,7 @@ package co.edu.uptc.automatonsimulationservice.ui.services;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ApplicationBootstrapService {
@@ -13,6 +14,7 @@ public class ApplicationBootstrapService {
             );
             Scene scene = new Scene(loader.load(), 1450, 780);
             stage.setTitle("SIMULADOR DE AUTÓMATAS FINITOS");
+            stage.getIcons().add(new Image(ApplicationBootstrapService.class.getResourceAsStream("/co/edu/uptc/automatonsimulationservice/logo.png")));
             stage.setScene(scene);
             stage.show();
         } catch (Exception exception) {
@@ -20,4 +22,3 @@ public class ApplicationBootstrapService {
         }
     }
 }
-
